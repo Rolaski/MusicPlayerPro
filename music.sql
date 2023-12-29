ab9c705644a8d9561ec0fe1c1390c8d484dd7a00
Jakub <jakubowskijjal1@gmail.com>
2023-12-29 09:04:11 +0100

    database update

--- music.sql
+++ music.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 26 Gru 2023, 09:20
+-- Czas generowania: 29 Gru 2023, 09:00
 -- Wersja serwera: 10.4.21-MariaDB
 -- Wersja PHP: 7.3.30
 
@@ -52,7 +52,16 @@ INSERT INTO `songs` (`id`, `track`, `musician`, `length`, `path`, `albumPath`) V
 (9, 'Adidas', 'Russian Village Boys & Mr. Polska', '00:02:15', '/com/example/songs/Adidas.mp3', '/com/example/albums/Adidas.jpg'),
 (10, 'Bungee', 'Hotel Maffija', '00:03:18', '/com/example/songs/Bungee.mp3', '/com/example/albums/Bungee.jpg'),
 (11, 'Good will hunting', 'Schafter', '00:03:01', '/com/example/songs/GoodWillHunting.mp3', '/com/example/albums/GoodWillHunting.jpg'),
-(12, 'OUT WEST', 'Travis Scott & JACKBOYS', '00:02:43', '/com/example/songs/OutWest.mp3', '/com/example/albums/OutWest.jpg');
+(12, 'OUT WEST', 'Travis Scott & JACKBOYS', '00:02:43', '/com/example/songs/OutWest.mp3', '/com/example/albums/OutWest.jpg'),
+(13, 'Gelenda', 'Avi', '00:02:38', '/com/example/songs/Gelenda.mp3', '/com/example/albums/Gelenda.jpg'),
+(14, 'Burn It ', 'Nickelback', '00:03:29', '/com/example/songs/BurnItToTheGround.mp3', '/com/example/albums/BurnItToTheGround.jpg'),
+(15, 'Ghost!', 'phonk.me & KIIXSHI', '00:03:51', '/com/example/songs/Ghost!.mp3', '/com/example/albums/Ghost!.jpg'),
+(16, 'Gangsta Paradise', 'Coolio', '00:04:01', '/com/example/songs/GangstaParadise.mp3', '/com/example/albums/GangstaParadise.jpg'),
+(17, 'Złote Tarasy', 'Mr. Polska', '00:02:33', '/com/example/songs/ZloteTarasy.mp3', '/com/example/albums/ZloteTarasy.jpg'),
+(18, 'I Love It', 'Icona Pop', '00:02:35', '/com/example/songs/ILoveIt.mp3', '/com/example/albums/ILoveIt.jpg'),
+(19, 'Highest In Room', 'Travis Scott', '00:02:57', '/com/example/songs/HighestInTheRoom.mp3', '/com/example/albums/HighestInTheRoom.jpg'),
+(20, 'We Are One', 'Pitbull', '00:03:46', '/com/example/songs/WeAreOne.mp3', '/com/example/albums/WeAreOne.jpg'),
+(21, 'The Ketchup Song', 'HELLFXRMANCE Remix', '00:03:04', '/com/example/songs/TheKetchupSong.mp3', '/com/example/albums/TheKetchupSong.jpg');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -72,7 +81,7 @@ ALTER TABLE `songs`
 -- AUTO_INCREMENT dla tabeli `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
