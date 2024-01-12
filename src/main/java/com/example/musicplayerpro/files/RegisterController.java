ed610ab315373d7dd9f1a538719892c588ab2326
Jakub <jakubowskijjal1@gmail.com>
2024-01-12 01:37:45 +0100

    add register section - controller class

---
+++ src/main/java/com/example/musicplayerpro/files/RegisterController.java
@@ -0,0 +1,166 @@
+package com.example.musicplayerpro.files;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.util.Objects;
+
+import static com.example.musicplayerpro.files.Main.sceneManager;
+
+public class RegisterController {
+
+    @FXML
+    private TextField name;
+    @FXML
+    private PasswordField password, passwordAgain;
+    @FXML
+    private CheckBox checkbox;
+    @FXML
+    private RadioButton radioButton1, radioButton2;
+    @FXML
+    private Label errorLabelUser, errorLabelPassword;
+    @FXML
+    private ImageView registerIcon;
+    @FXML
+    private ImageView previous;
+
+
+    @FXML
+    void previousDefault()
+    {
+        Image defaultImage = new Image(Objects.requireNonNull(getClass().getResource("/com/example/images/previous.png")).toExternalForm());
+        previous.setImage(defaultImage);
+    }
+    @FXML
+    void previousHover()
+    {
+        Image hoverImage = new Image(Objects.requireNonNull(getClass().getResource("/com/example/images/previousHover.png")).toExternalForm());
+        previous.setImage(hoverImage);
+    }
+    @FXML
+    void registerDefault(MouseEvent event)
+    {
+        Image registerDefault = new Image(Objects.requireNonNull(getClass().getResource("/com/example/images/register.png")).toExternalForm());
+        registerIcon.setImage(registerDefault);
+    }
+    @FXML
+    void registerHover(MouseEvent event)
+    {
+        Image registerHover = new Image(Objects.requireNonNull(getClass().getResource("/com/example/images/registerHover.png")).toExternalForm());
+        registerIcon.setImage(registerHover);
+    }
+
+    @FXML
+    private void initialize()
+    {
+        ToggleGroup radioButtonGroup = new ToggleGroup();
+        radioButton1.setToggleGroup(radioButtonGroup);
+        radioButton2.setToggleGroup(radioButtonGroup);
+
+        checkbox.setOnAction(event -> {
+            if (checkbox.isSelected()) {
+                passwordAgain.setDisable(true);
+                passwordAgain.setPromptText(passwordAgain.getText());
+                passwordAgain.setText("");
+            } else {
+                passwordAgain.setDisable(false);
+                passwordAgain.setText(passwordAgain.getPromptText());
+                passwordAgain.setPromptText("Password");
+            }
+        });
+    }
+    //method for passwords validation
+    private boolean checkUserValidity()
+    {
+        String nameText = name.getText();
+
+        if (nameText.isEmpty())
+        {
+            errorLabelUser.setText("Pole 'Name' nie może być puste");
+            return false;
+        }
+        else if (!isValidUserName(nameText))
+        {
+            errorLabelUser.setText("Nieprawidłowe znaki w polu 'Name'");
+            return false;
+        }
+        else
+        {
+            errorLabelUser.setText("");
+            return true;
+        }
+    }
+
+    private boolean isValidUserName(String userName)
+    {
+        //accepted characters are: a-z, A-Z, 0-9, _-?!
+        return userName.matches("[a-zA-Z0-9_\\-!\\?]+");
+    }
+
+    //method for passwords validation
+    private boolean checkPasswordValidity()
+    {
+        String passwordText = password.getText();
+        String passwordAgainText = passwordAgain.getText();
+
+        if (passwordText.isEmpty() || passwordAgainText.isEmpty())
+        {
+            errorLabelPassword.setText("Pole nie może być puste");
+            return false;
+        }
+        else if (!passwordText.equals(passwordAgainText))
+        {
+            errorLabelPassword.setText("Hasła są różne");
+            return false;
+        }
+        else if (passwordText.length() < 6)
+        {
+            errorLabelPassword.setText("Hasło musi mieć co najmniej 5 znaków");
+            return false;
+        }
+        else if (!containsSpecialCharacter(passwordText))
+        {
+            errorLabelPassword.setText("Hasło musi zawierać co najmniej 1 znak specjalny");
+            return false;
+        }
+        else
+        {
+            errorLabelPassword.setText("");
+            return true;
+        }
+    }
+    //method for special characters in password
+    private boolean containsSpecialCharacter(String password)
+    {
+        String specialCharacters = "!@#$%^&*()-_=+[]{}|;:'\",.<>/?";
+        for (char c : password.toCharArray())
+        {
+            if (specialCharacters.contains(String.valueOf(c))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //method for check validation
+    //if all is correct -> add user to database
+    @FXML
+    private void registerSuccessful(ActionEvent event)
+    {
+        if(checkUserValidity() && checkPasswordValidity())
+        {
+            errorLabelUser.setText("Rejestracja udana!");
+        }
+    }
+
+    //method to return to the previous page
+    @FXML
+    private void goToLoginPanel(ActionEvent event)
+    {
+        sceneManager.switchScene("LoginController");
+    }
+}
