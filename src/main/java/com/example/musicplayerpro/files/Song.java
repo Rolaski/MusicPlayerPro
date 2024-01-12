64008e8f72d5eee5ac61a9782bb4df48a90a5cbe
Jakub <jakubowskijjal1@gmail.com>
2024-01-12 01:32:36 +0100

    adding relation with musician entity

--- src/main/java/com/example/musicplayerpro/files/Song.java
+++ src/main/java/com/example/musicplayerpro/files/Song.java
@@ -13,26 +13,32 @@ public class Song
     private int id;
     @Column(name = "track")
     private String track;
-    @Column(name = "musician")
-    private String musician;
     @Column(name = "length")
     private java.time.LocalTime songLength;
-
     @Column(name = "path")
     private String path;
     @Column(name = "albumPath")
     private String albumPath;
 
+    @ManyToOne
+    @JoinColumn(name = "fk_musician")
+    Musician musician;
+
     public Song() {
     }
 
-    public Song(int id, String track, String musician, LocalTime songLength, String path, String albumPath) {
+    public Song(int id, String track, LocalTime songLength, String path, String albumPath, Musician musician)
+    {
         this.id = id;
         this.track = track;
-        this.musician = musician;
         this.songLength = songLength;
         this.path = path;
         this.albumPath = albumPath;
+        this.musician = musician;
+    }
+
+    public String getMusician() {
+        return musician.getMusicianName();
     }
 
     public String getAlbumPath()
@@ -44,15 +50,10 @@ public class Song
         return id;
     }
 
-
     public String getTrack() {
         return track;
     }
 
-
-    public String getMusician() {
-        return musician;
-    }
 
     public LocalTime getSongLength() {
         return songLength;
@@ -61,5 +62,8 @@ public class Song
     public String getPath() {
         return path;
     }
+
+
+
 
 }
